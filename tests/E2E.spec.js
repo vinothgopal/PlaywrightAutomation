84cf8c4823d5260e619c60cb23e62e831fc79b9a
Sreelakshmi <totkarsreelakshmi@gmail.com>
2025-05-04 16:59:40 +0530

    chore: added e2e test

--- tests/E2E.spec.js
+++ tests/E2E.spec.js
@@ -1,19 +1,20 @@
 // @ts-check
 import { test, expect } from '@playwright/test';
+const {POManager} = require('../pageobject/POManager');
+const dataset= JSON.parse(JSON.stringify(require('../utils/test.json')));
+const{config } =require("dotenv")
 
-test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+test('E22 TC1', async ({ page }) => {
+  const poManager = new POManager(page);
+  const loginPage = poManager.getLoginPage();
+  const homePage = poManager.getHomePage();
+                    await loginPage.navigate(dataset.url);
+  await expect(page).toHaveTitle("Test Login Page for Automation Testing Practice")
+    await loginPage.login(dataset.username, dataset.password);
+  expect (await homePage.getWelcomeMessage()).toEqual("Secure Area page for Automation Testing Practice");
+  await expect(page).toHaveTitle("Secure Page page for Automation Testing Practice")
+    await homePage.clickLogoutButton();
+  await expect(page).toHaveTitle("Test Login Page for Automation Testing Practice")
 
   // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
-});
-
-test('get started link', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
-
-  // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
-
-  // Expects page to have a heading with the name of Installation.
-  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
 });
