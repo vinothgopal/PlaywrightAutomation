b09899267b285cba0025c4a7db97881ee8d52658
Sreelakshmi <totkarsreelakshmi@gmail.com>
2025-05-04 22:15:10 +0530

    chore: added eslint rule

--- eslint.config.js
+++ eslint.config.js
@@ -1,23 +1,23 @@
-import noOnlyTests from 'eslint-plugin-no-only-tests';
-import prettierPlugin from 'eslint-plugin-prettier';
+import noOnlyTests from "eslint-plugin-no-only-tests";
+import prettierPlugin from "eslint-plugin-prettier";
 
 export default [
   {
-    files: ['**/*.js'],
-    ignores: ['node_modules', 'dist', 'playwright-report'],
+    files: ["**/*.js"],
+    ignores: ["node_modules", "dist", "playwright-report"],
     languageOptions: {
-      ecmaVersion: 'latest',
-      sourceType: 'module',
+      ecmaVersion: "latest",
+      sourceType: "module",
     },
     plugins: {
-      'no-only-tests': noOnlyTests,
+      "no-only-tests": noOnlyTests,
       prettier: prettierPlugin,
     },
     rules: {
-      'no-only-tests/no-only-tests': 'error',
-      'prettier/prettier': 'error',
-      'no-unused-vars': 'warn',
-      'no-console': 'off'
+      "no-only-tests/no-only-tests": "error", // Disallow .only() in test files
+      "prettier/prettier": "error", // Ensure Prettier formatting is followed
+      "no-unused-vars": "warn", // Warn about unused variables
+      "no-console": "off", // Allow console usage
     },
   },
 ];
